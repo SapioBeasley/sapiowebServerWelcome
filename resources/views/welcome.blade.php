97022c47575be138505b6c8f52d4790e5de8ca21
Andreas Beasley <andreas.beasley@gmail.com>
2016-08-18 23:15:58 -0700

    initial

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en-us" class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome to SapioWeb</title>
+
+        <link rel="shortcut icon" href="img/favicon.png">
+        <link rel="stylesheet" href="css/style-flat.css" />
+        <script src="js/modernizr.js"></script>
+    </head>
+
+    <body>
+        <div id="container">
+            <div class="overlay"></div>
+            <div class="item-title">
+                <div id="message"></div>
+                <div class="link-bottom">
+                    <a class="link-icon" href="https://sapioweb.com">
+                        <i class="icon ion-ios-home"></i> HOME
+                    </a>
+                    <a class="link-icon" href="mailto:andreas@sapioweb.com">
+                        <i class="icon ion-ios-compose"></i> WRITE US
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <script src="js/jquery-1.12.3.min.js"></script>
+        <script src="js/jquery.easing.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/main-flat.js"></script>
+
+        <!--[if lt IE 10]><script type="text/javascript" src="js/placeholder.js"></script><![endif]-->
+    </body>
+</html>
